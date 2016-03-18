34811ad53a4432948376117ebfb4197a8c72e889
Shawn Michaud <shawnmne@hotmail.com>
2016-03-17 19:57:19 -0500

    most validated

--- lib/password_validator.rb
+++ lib/password_validator.rb
@@ -2,8 +2,12 @@ require "pry"
 
 def valid_password?(password)
   at_least_one_caps?(password) && 
-  at_least_eight_characters?(password) # &&
-  # at_least_one_lower_case?(password)
+  at_least_eight_characters?(password) &&
+  at_least_one_lower?(password) &&
+  at_least_one_number?(password) &&  
+  !cant_contain_password?(password) &&
+  !cant_contain_name?(password)
+
 end
 
 # Takes a string
@@ -15,4 +19,26 @@ end
 
 def at_least_eight_characters?(password)
   password.length >= 8
+end
+
+def at_least_one_lower?(password)
+	password != password.upcase
+end
+
+def at_least_one_number?(password)
+	if password.match("[0123456789]") 
+		return true
+	end
+end	
+
+def cant_contain_password?(password)
+	if password.match('password') 
+    true
+	end
+end
+
+def cant_contain_name?(password)
+	if password.match('shawn')
+		true
+	end
 end
