ae3428581c59949f8661ec56c44307db2c09a4c2
Shawn Michaud <shawnmne@hotmail.com>
2016-03-17 19:58:33 -0500

    most tests complete

--- tests/password_validator_test.rb
+++ tests/password_validator_test.rb
@@ -31,4 +31,52 @@ class PasswordValidatorTest < Minitest::Test
     result = valid_password?("1Abils&a")
     assert(result, "'1Abils&a' should be valid")
   end
+
+# Try a password with all-upper-case-letters
+  def test_reject_all_upper_case_passwords
+    result = at_least_one_lower?("1ABJILS&A")
+    refute(result, "'1ABJILS&A' should be invalid because it contains no caps")
+  end
+
+# Try a password with mixed cases
+  def test_accepts_mixed_case_passwords_lower_case
+    result = at_least_one_lower?("1Abils&a")
+    assert(result, "'1Abils&a' should be vaild because it is mixed case")
+  end
+
+# Try a password with no numbers
+  def test_reject_no_numbers_password
+    result = at_least_one_number?("Abils&a")
+    refute(result, "'Abils&a' should be invalid because it contains no numbers")
+  end 
+
+#try a password with numbers
+  def test_accept_numbers_in_password
+    result = at_least_one_number?("1Abils&a")
+    assert(result,"'1Abils&a' should be valid because it has a number")
+  end
+
+# try a password with "password" in it
+  def test_reject_password_in_password
+    result = !cant_contain_password?("abpassword")
+    refute(result, "'abpassword' should be invalid because it contains 'password'")
+  end
+
+ #try a valid password
+  def test_accept_no_password_in_password
+    result = !cant_contain_password?("1Abils&a")
+    assert(result,"'1Abils&a' should be valid because it does not contain 'password'")
+  end 
+
+# try a password with my name
+  def test_reject_name_in_password
+    result = !cant_contain_name?("1abshawn")
+    refute(result,"'1abshawn' should be invalid because it contains my name")
+  end
+
+#try a password without my name
+  def test_accept_good_password
+    result = !cant_contain_name?("1Abils&a")
+    assert(result,"'1Abils&a' should be valid because it does not contain my name")
+   end   
 end
